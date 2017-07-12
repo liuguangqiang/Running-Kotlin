f2129f9b12bd2c3f105903ba740d40d824de34bc
Eric <guangqiang.liu@symbio.com>
2017-07-12 13:44:53 +0800

    [ci skip] Build bumped to 2

--- app/src/main/java/com/liuguangqiang/irunning/app/RunningApplication.kt
+++ app/src/main/java/com/liuguangqiang/irunning/app/RunningApplication.kt
@@ -1,11 +1,11 @@
 package com.liuguangqiang.irunning.app
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.liuguangqiang.support.utils.Logger
+import io.fabric.sdk.android.Fabric
 import io.realm.Realm
 import io.realm.RealmConfiguration
-
-
 
 /**
  * Created by Eric on 2017/5/19.
@@ -15,6 +15,12 @@ class RunningApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Logger.setTag("running")
+        val fabric: Fabric = Fabric.Builder(this)
+                .kits(Crashlytics())
+                .debuggable(true)
+                .build()
+        Fabric.with(fabric)
+
         Realm.init(this)
         val realmConfig = RealmConfiguration.Builder()
                 .deleteRealmIfMigrationNeeded()
